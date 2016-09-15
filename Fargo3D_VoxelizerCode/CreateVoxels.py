95118301c1e1cc9590085f50c9e1ebd050e1a3d8
matgarate <matgarate@gmail.com>
2016-09-15 03:06:15 -0300

    AddedTrilinearInterpolation

--- Fargo3D_VoxelizerCode/CreateVoxels.py
+++ Fargo3D_VoxelizerCode/CreateVoxels.py
@@ -6,5 +6,5 @@ OutputFolder="VoxelData/"
 
 for i in range(638):
 	print "Snap: "+str(i)
-	command="./bvoxer -in "+InputFolder+"gasdens"+str(i)+".dat -out "+OutputFolder+"gasdens"+str(i)+".bvox -ns 512 -nr 256 -nc 64 -vnx 500 -vny 500 -vnz 100 -logr -logv"
+	command="./bvoxer -in "+InputFolder+"gasdens"+str(i)+".dat -out "+OutputFolder+"gasdens"+str(i)+".bvox -ns 512 -nr 256 -nc 64 -vnx 500 -vny 500 -vnz 100 -logr -logv -tri"
 	os.system(command)
--- Fargo3D_VoxelizerCode/bvoxer_Fargo3D.c
+++ Fargo3D_VoxelizerCode/bvoxer_Fargo3D.c
@@ -19,6 +19,7 @@ int nc;
 
 int logscale=FALSE;
 int logvalues=FALSE;
+int tri=FALSE;
 
 //Voxel resolution
 int nx = 400;
@@ -28,30 +29,94 @@ int nz = 100;
 real *data;
 float *vdata;
 
-real *r_med;
-real *theta_med;
-real *phi_med;
 
 
-real GetCartesianValue(real r, real phi, real theta){
+real *r;
+real *theta;
+real *phi;
+
+
+real trilinear_interpolation(real x0, real x1,
+			      real y0, real y1,
+			      real z0, real z1,
+			      real c000, real c100, real c110, real c010,
+			      real c001, real c101, real c111, real c011,
+			      real x, real y, real z) {
+
+  //The cube is defined by the vertices (x0,y0,z0), (x1,y1,z1)
+  real xd,yd,zd;
+  real c00, c01, c10, c11;
+  real c0, c1;
+  real c;
+
+  xd = (x-x0)/(x1-x0);
+  yd = (y-y0)/(y1-y0);
+  zd = (z-z0)/(z1-z0);
+
+  // Linear interpolation along x
+  c00 = c000*(1.0-xd) + c100*xd;
+  c01 = c001*(1.0-xd) + c101*xd;
+  c10 = c010*(1.0-xd) + c110*xd;
+  c11 = c011*(1.0-xd) + c111*xd;
+  // Now linear interpolation along y
+  c0  = c00*(1.0-yd) + c10*yd;
+  c1  = c01*(1.0-yd) + c11*yd;
+
+  // Finally, linear interpolation along z
+  return (c0*(1-zd) + c1*zd);
+}
+
+
+
+real GetCartesianValue(real r_cell, real phi_cell, real theta_cell){
 //This function is returning the closest value in the data array to the real spherical coordinates.
   real value = 0;
 
   int i,j,k;
 
-  i = (int)((phi-phi_med[0])/(phi_med[ns-1]-phi_med[0])*ns);
-  j = (int)((r-r_med[0])/(r_med[nr-1]-r_med[0])*nr);
+  i = (int)((phi_cell-phi[0])/(phi[ns]-phi[0])*ns);
+  j = (int)((r_cell-r[0])/(r[nr]-r[0])*nr);
 
   if(logscale)
-  	j = (int)(nr*log10(r/r_med[0])/log10(r_med[nr-1]/r_med[0]));
+  	j = (int)(nr*log10(r_cell/r[0])/log10(r[nr]/r[0]));
+  k = (int)((theta_cell-theta[0])/(theta[nc]-theta[0])*nc);
+
+  if(j>=0 && k>=0 && i>=0 && j<nr && k<nc) {
+
+    if(tri)
+    {
+        int ip=i+1;
+        int jp=j+1;
+        int kp=k+1;
+
+        if(ip>=ns) ip=ip-ns;
+        if(jp>=nr-1) jp=j;
+        if(kp>=nc-1) kp=k;
+
+        //For these values you are using the midpoints.
+        //So go from i=0->ns-1  j=0->nr-1 k=0->nc-1, same for ip(cyclic),jp,kp
+        real v000=data[i+j*ns+k*ns*nr];
+        real v100=data[ip+j*ns+k*ns*nr];
+        real v110=data[ip+jp*ns+k*ns*nr];
+        real v010=data[i+jp*ns+k*ns*nr];
+        real v001=data[i+j*ns+kp*ns*nr];
+        real v101=data[ip+j*ns+kp*ns*nr];
+        real v111=data[ip+jp*ns+kp*ns*nr];
+        real v011=data[i+jp*ns+kp*ns*nr];
+
+        //For here your are using the boundaries so i=0->ns-1  j=0->nr-1 k=0->nc-1, same for i+1,j+1,k+1
+        value = trilinear_interpolation(phi[i],phi[i+1],
+                                        r[j],r[j+1],
+                                        theta[k],theta[k+1],
+                                        v000,v100,v110,v010,
+                                        v001,v101,v111,v011,
+                                        phi_cell,r_cell,theta_cell);
+
+    }
+    else
+        value = data[i+j*ns+k*ns*nr];
 
 
-  k = (int)((theta-theta_med[0])/(theta_med[nc-1]-theta_med[0])*nc);
-
-  if(j>=0 && k>=0 && i>=0 && j<nr-1 && k<nc-1) {
-    if (i >= ns-1)
-      i = i-(ns-1);
-    value = data[i+j*ns+k*ns*nr];
   } else
     value = 0.0;
 
@@ -72,11 +137,13 @@ void usage()
     fprintf(stderr,"[-nc <int>] Number of colatitude sections in Fargo3D simulation.\n");
 
     fprintf(stderr,"[-vnx <int>] Blender voxel resolution in the x axis. Default: 400.\n");
-    fprintf(stderr,"[-vnx <int>] Blender voxel resolution in the y axis. Default: 400.\n");
-    fprintf(stderr,"[-vnx <int>] Blender voxel resolution in the z axis. Default: 100.\n");
+    fprintf(stderr,"[-vny <int>] Blender voxel resolution in the y axis. Default: 400.\n");
+    fprintf(stderr,"[-vnz <int>] Blender voxel resolution in the z axis. Default: 100.\n");
 
     fprintf(stderr,"[-logr] Fargo simulation radial coordinate is in logscale.\n");
     fprintf(stderr,"[-logv] Output fargo field values in logscale.\n");
+    fprintf(stderr,"[-tri] Use Trilinear Interpolation.\n");
+
 
     exit(0);
 }
@@ -150,13 +217,16 @@ int main(int argc, char **argv)
 			filename_out= argv[i];
 			++i;
 			}
-
 		else if (!strcmp(argv[i],"-logr")) {
 			logscale=TRUE;
 			++i;
 			}
 		else if (!strcmp(argv[i],"-logv")) {
 			logvalues=TRUE;
+			++i;
+			}
+		else if (!strcmp(argv[i],"-tri")) {
+			tri=TRUE;
 			++i;
 			}
         else usage();
@@ -169,13 +239,15 @@ int main(int argc, char **argv)
 
     real rr,ttheta,pphi;
 
-    real *r;
-    real *theta;
-    real *phi;
 
     real *x;
     real *y;
     real *z;
+
+    real *r_med;
+    real *theta_med;
+    real *phi_med;
+
 
     real dummy;
 
@@ -266,6 +338,11 @@ int main(int argc, char **argv)
       if(data[k]<min_value) min_value = data[k];
 
     }
+
+
+    //min_value=-16.85;
+    //max_value=-4.8;
+
     printf("MaxValue: %lf\n", max_value);
     printf("MinValue: %lf\n", min_value);
 
